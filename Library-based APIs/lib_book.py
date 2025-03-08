0c0091e653492fce130f56d238d4bc5830082add
Harish Raj <harishrajs0808@gmail.com>
2025-03-08 19:02:59 +0530

    Add files via upload
    Simple api's with easy example code in python

---
+++ Library-based APIs/lib_book.py
@@ -0,0 +1,19 @@
+from simpleapi import Bookstore
+
+class LibraryManager:
+    def __init__(self, bookstore):
+        self.bookstore = bookstore
+
+    def add_book_to_store(self, id, title, author, year, genre):
+        new_book = self.bookstore.add_book(id, title, author, year, genre)
+        return new_book
+    
+if __name__ == "__main__" :
+    bookstore = Bookstore()
+
+    manager = LibraryManager(bookstore)
+
+    manager.add_book_to_store(1, "The Great Gatsby", "F. Scott Fitzgerald", 1925, "Fiction")
+    manager.add_book_to_store(2, "To Kill a Mockingbird","Harper Lee", 1968, "Fiction")
+
+    print(bookstore.get_book_by_id(2)["title"])
